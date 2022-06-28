64b1393ec26c0ba3e5370d034fd75490c4bda64a
Federico Grandi <fgrandi30@gmail.com>
2022-06-28 13:34:47 +0000

    chore: add linters and ts deps

---
+++ .eslintrc.js
@@ -0,0 +1,38 @@
+module.exports = {
+  env: {
+    commonjs: true,
+    es6: true,
+    node: true
+  },
+  extends: ['eslint:recommended', 'prettier', 'plugin:prettier/recommended'],
+  globals: {},
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 9,
+    sourceType: 'module'
+  },
+  plugins: ['@typescript-eslint', 'jest'],
+  rules: {
+    'prettier/prettier': 'warn',
+    'no-cond-assign': ['error', 'except-parens'],
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': 'warn',
+    'no-redeclare': 'off',
+    '@typescript-eslint/no-redeclare': 'warn',
+    'no-dupe-class-members': 'off',
+    '@typescript-eslint/no-dupe-class-members': 'warn',
+    'no-empty': [
+      'error',
+      {
+        allowEmptyCatch: true
+      }
+    ],
+    'prefer-const': [
+      'warn',
+      {
+        destructuring: 'all'
+      }
+    ],
+    'spaced-comment': 'warn'
+  }
+}
